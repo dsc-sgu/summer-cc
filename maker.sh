6b6d59b711e97b5b4f682eaad8a7724c3aada868
Andrew Guschin <guschin.drew@gmail.com>
2022-07-12 12:27:54 +0400

    Добавил правило для создания проекта на Windows в maker.sh

--- maker.sh
+++ maker.sh
@@ -7,6 +7,12 @@ build() {
     && ninja -C ./Build
 }
 
+winbuild() {
+    set -o xtrace
+	cmake -DBUILD_SHARED_LIBS=False \
+          -G "Visual Studio 16 2019" -S . -B ./Build
+}
+
 clean() {
     set -o xtrace
 	rm -rf ./Build/*
@@ -15,11 +21,13 @@ clean() {
 help() {
 	echo "Использование:"
 	echo "./maker.sh build -> Собрать проект с помощью CMake и ninja -C ./Build/"
+	echo "./maker.sh winbuild -> Сгенерировать проектные файлы для Visual Studio 16 2019 в директорию ./Build/"
 	echo "./maker.sh clean -> Удалить содержимое директории ./Build/"
 }
 
 case "$1" in
     build) build ;;
+    winbuild) winbuild ;;
     clean) clean ;;
     *) help ;;
 esac
