8a85f00bcbab04a38befc743a20d44e606358325
Daniil <91054142+danko382@users.noreply.github.com>
2022-07-15 13:26:10 +0400

    Add component system (#1)
    * Примитивная система компонентов.
    
    * Добавил Transform
    
    * Добавили Sprite

---
+++ Source/components.h
@@ -0,0 +1,75 @@
+#include <vector>
+#include <string>
+#include <typeinfo>
+#include <iostream>
+#include <typeindex>
+#include <raylib-ext.hpp>
+typedef int Entity_id;
+
+class Storage;
+
+class Component 
+{
+public:
+
+    virtual void Update(float dt, Entity_id parent_id, Storage &storage) = 0;
+    virtual std::string get_component_type() = 0;
+};
+
+class Entity 
+{
+public:
+
+    Entity_id id;
+    std::vector<Component*> components;
+    template <typename T> 
+    T* getComponent()
+    {
+
+        for (int i = 0; i < components.size(); i++) {
+            
+            if(std::type_index(typeid(T)).name() == components[i]->get_component_type()) {
+                return (T*) components[i];
+            } 
+        }
+
+        return nullptr;
+    }
+};
+
+class Storage
+{
+public:
+
+    std::vector<Entity>entitys;
+};
+
+class _Sprite : public Component 
+{
+public:
+
+    _Sprite(const std::string& path);
+    void Update(float dt, Entity_id parent_id, Storage &storage) override;
+    std::string get_component_type();
+    Texture2D texture;
+    Image image;
+};
+
+class _Transform : public Component 
+{
+public:
+
+    void Update(float dt, Entity_id parent_id, Storage &storage) override;
+    std::string get_component_type();
+    Vector3 pos;
+    float angle; 
+};
+
+class _Player_control : public Component 
+{
+public:
+
+    void Update(float dt, Entity_id parent_id, Storage &storage) override;
+    std::string get_component_type();
+    int speed;
+};
--- Source/main.cpp
+++ Source/main.cpp
@@ -1,27 +1,47 @@
 #define RAYEXT_IMPLEMENTATION
 #include <raylib-ext.hpp>
+#include "components.h"
+#include <iostream>
+
+Storage storage;
 
 int main()
 {
+
     const int screenWidth = 640;
     const int screenHeight = 480;
-
     InitWindow(screenWidth, screenHeight, "GDSC: Creative Coding");
     SetTargetFPS(60);
+
+    storage.entitys.push_back(Entity());
+    _Player_control *player_control = new _Player_control();
+    _Transform *trnsf = new _Transform();
+    _Sprite *spr = new _Sprite("Asssets/Textures/King_stay.png");
+    player_control->speed = 100;
+    storage.entitys[0].components.push_back(player_control);
+    storage.entitys[0].components.push_back(trnsf);
+    storage.entitys[0].components.push_back(spr);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         {
             ClearBackground(BLACK);
-            int fontSize = 30;
-            std::string msg = "Welcome, Creative Coders!";
-            int x = (screenWidth - MeasureText(msg, fontSize)) / 2;
-            int y = (screenHeight - fontSize) / 2;
-            DrawText(msg, x, y, fontSize, WHITE);
+
+            for(int i = 0; i<storage.entitys.size(); i++){
+                for(int j = 0; j<storage.entitys[i].components.size(); j++){
+
+                    storage.entitys[i].components[j]->Update(GetFrameTime(),storage.entitys[i].id,storage);
+                    auto t = storage.entitys[i].getComponent<_Transform>();
+                    auto s = storage.entitys[i].getComponent<_Sprite>();
+                    DrawTexture(s->texture, t->pos.x, t->pos.y, WHITE);
+                }   
+            }
         }
+
         EndDrawing();
     }
+    
     CloseWindow();
 
     return 0;
