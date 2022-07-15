af6a4c797a6f2127efa8decfef881d9fc7e18cf4
Alex <grazhdanov3002@mail.ru>
2022-07-15 17:00:01 +0400

    Добавил test.json

--- Source/main.cpp
+++ Source/main.cpp
@@ -3,24 +3,66 @@
 #include "components.h"
 #include <iostream>
 
+#include <nlohmann/json.hpp>
+#include <fstream>
+
+using json = nlohmann::json;
+
+Storage load_lvl(std::string path) {
+    Storage new_lvl;
+    json lvl_json;
+    std::ifstream lvl_file(path);
+    
+    int n = lvl_json["count"];
+
+    for (int i = 0; i < n; i++) {
+        new_lvl.entitys.push_back(Entity());
+
+        auto entity = lvl_json["entities"][i];
+        for(int j = 0; j<3; j++)
+        if (entity[j]["type"] == "Transform") {
+            _Transform* transform = new _Transform();
+            transform->angle = entity[j]["angle"];
+            transform->pos = Vector3{ entity[j]["pos"]["x"], entity[j]["pos"]["y"], entity[j]["pos"]["z"] };
+            new_lvl.entitys[i].components.push_back(transform);
+        }
+        else if (entity[j]["type"] == "PlayerControl") {
+            _Player_control* player_control = new _Player_control();
+            player_control->speed = entity[j]["speed"];
+            new_lvl.entitys[i].components.push_back(player_control);
+        }
+        else if (entity[j]["type"] == "Sprite") {
+            _Sprite* sprite = new _Sprite(entity[j]["path"]);
+            new_lvl.entitys[i].components.push_back(sprite);
+        }
+    }
+
+
+    lvl_file.close();
+
+    return new_lvl;
+}
+
 Storage storage;
 
 int main()
 {
-
+   
     const int screenWidth = 640;
     const int screenHeight = 480;
     InitWindow(screenWidth, screenHeight, "GDSC: Creative Coding");
     SetTargetFPS(60);
 
-    storage.entitys.push_back(Entity());
+    /*storage.entitys.push_back(Entity());
     _Player_control *player_control = new _Player_control();
     _Transform *trnsf = new _Transform();
     _Sprite *spr = new _Sprite("Asssets/Textures/King_stay.png");
     player_control->speed = 100;
     storage.entitys[0].components.push_back(player_control);
     storage.entitys[0].components.push_back(trnsf);
-    storage.entitys[0].components.push_back(spr);
+    storage.entitys[0].components.push_back(spr);*/
+
+    storage = load_lvl("Asssets/Lvls/test.json");
 
     while (!WindowShouldClose())
     {
