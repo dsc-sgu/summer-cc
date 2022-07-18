8a0b18d4732ddb50f9069fe718c6ee3461501fcb
Alex <grazhdanov3002@mail.ru>
2022-07-18 13:12:28 +0400

    json loader

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,44 +2,13 @@
 #include <raylib-ext.hpp>
 //#include "components.h"
 #include "json_loader.h"
+#include "render.h"
 #include <iostream>
 
 
 
 
 Storage storage;
-
-struct Viewport
-{
-    float left, right;
-    float bottom, top;
-};
-
-Vector2
-screen_to_local(Viewport &viewport, Vector2 screen_size, Vector2 point)
-{
-    Vector2 d = {
-        viewport.right - viewport.left,
-        viewport.top - viewport.bottom
-    };
-    return Vector2 {
-        (point.x / screen_size.x) * d.x + viewport.left,
-        (point.y / screen_size.y) * d.y + viewport.bottom,
-    };
-}
-
-void
-set_viewport(Viewport &viewport, Vector2 screen_size, Rectangle rect)
-{
-    Vector2 p0 = screen_to_local(viewport, screen_size, Vector2 { rect.x, rect.y });
-    Vector2 p1 = screen_to_local(viewport, screen_size, Vector2 { rect.x + rect.width, rect.y + rect.height });
-    viewport = Viewport {
-        p0.x,
-        p1.x,
-        p0.y,
-        p1.y,
-    };
-}
 
 
 
