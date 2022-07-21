0564dafd2af94a32d068f612c0d4f291bbf779c2
Michael Chernigin <59616661+mchernigin@users.noreply.github.com>
2022-07-21 11:34:34 +0400

    Добавил камеру (#5)
    * Почистил код
    * Добавил возможность масштабирования текстур
    * Добавил namespace игры "plat"
    * Добавил камеру
    
    Co-authored-by: danko328 <daniil-kondrashov-rabotaet@yandex.ru>
    Co-authored-by: Dankoveshnikov <dan-acer@outlook.com>

--- Source/components.cpp
+++ Source/components.cpp
@@ -1,56 +1,111 @@
 #include "components.h"
 
-typedef int Entity_id;
+namespace plat {
 
-std::string _Player_control::get_component_type()
+Entity::Entity()
 {
-    return std::type_index(typeid(_Player_control)).name();
+    static Entity_id count = 0;
+    id = count++;
 }
 
-std::string _Transform::get_component_type()
+std::string
+Transform::get_component_type()
 {
-    return std::type_index(typeid(_Transform)).name();
+    return std::type_index(typeid(Transform)).name();
 }
 
-void _Transform::Update(float dt, Entity_id parent_id, Storage &storage)
+void
+Transform::update(float dt, Entity_id parent_id, Storage &storage)
 {}
 
-void _Sprite::Update(float dt, Entity_id parent_id, Storage &storage)
-{}
-
-std::string _Sprite::get_component_type()
-{
-    return std::type_index(typeid(_Sprite)).name();
-}
-
-_Sprite::_Sprite(const std::string &path)
+Sprite::Sprite(const std::string &path)
 {
     image = LoadImage(path);
     texture = LoadTextureFromImage(image);
 }
 
-_Sprite::_Sprite()
+std::string
+Sprite::get_component_type()
+{
+    return std::type_index(typeid(Sprite)).name();
+}
+
+void
+Sprite::update(float dt, Entity_id parent_id, Storage &storage)
 {}
 
-void _Player_control::Update(float dt, int parent_id, Storage &storage)
+std::string
+Player_control::get_component_type()
 {
-    auto cur_transform = storage.entities[parent_id].getComponent<_Transform>();
-    
+    return std::type_index(typeid(Player_control)).name();
+}
+
+void
+Player_control::update(float dt, int parent_id, Storage &storage)
+{
+    auto cur_transform = storage.entities[parent_id].getComponent<Transform>();
+
     if (IsKeyDown(KEY_W))
     {
         cur_transform->pos.y += speed * dt;
     }
-    else if (IsKeyDown(KEY_S))
+    if (IsKeyDown(KEY_S))
     {
         cur_transform->pos.y -= speed * dt;
     }
-    
     if (IsKeyDown(KEY_A))
     {
         cur_transform->pos.x -= speed * dt;
     }
-    else if (IsKeyDown(KEY_D))
+    if (IsKeyDown(KEY_D))
     {
         cur_transform->pos.x += speed * dt;
     }
+}
+
+std::string
+Camera::get_component_type()
+{
+    return std::type_index(typeid(Camera)).name();
+}
+
+void
+Camera::update(float dt, Entity_id parent_id, Storage &storage)
+{
+    plat::Transform *t = storage.entities[parent_id].getComponent<Transform>();
+    plat::Camera *cam = storage.entities[parent_id].getComponent<Camera>();
+    if (IsKeyDown(KEY_LEFT))
+    {
+        t->pos.x -= 1;
+    }
+    if (IsKeyDown(KEY_RIGHT))
+    {
+        t->pos.x += 1;
+    }
+    if (IsKeyDown(KEY_UP))
+    {
+        if (IsKeyDown(KEY_LEFT_SHIFT))
+        {
+            cam->scale.x *= 1.25;
+            cam->scale.y *= 1.25;
+        }
+        else
+        {
+            t->pos.y += 1;
+        }
+    }
+    if (IsKeyDown(KEY_DOWN))
+    {
+        if (IsKeyDown(KEY_LEFT_SHIFT))
+        {
+            cam->scale.x *= 1 / 1.25;
+            cam->scale.y *= 1 / 1.25;
+        }
+        else
+        {
+            t->pos.y -= 1;
+        }
+    }
+}
+
 }
