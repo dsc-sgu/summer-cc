dbef43a926caf104fd177180b740cf36cfc95d4a
Dankoveshnikov <dan-acer@outlook.com>
2022-07-21 18:31:53 +0300

    Отрисовка не в том месте

--- Source/json_loader.cpp
+++ Source/json_loader.cpp
@@ -59,30 +59,39 @@ load_lvl(std::string path)
             }
             else if (component["type"] == "Physics")
             {
+               
+                plat::Transform *trans = new_lvl.entities.back().getComponent<plat::Transform>();
+                plat::Sprite *spr = new_lvl.entities.back().getComponent<plat::Sprite>();
+                plat::Camera *cam = new_lvl.entities[new_lvl.cur_camera].getComponent<plat::Camera>();
+
+                plat::Physics *cur_phys = new plat::Physics();
+
+                int sprite_width = spr->image.width * trans->scale.x * cam->scale.x;
+                int sprite_height = spr->image.height * trans->scale.y * cam->scale.y;
+                cur_phys->bodyDef.position.Set(trans->pos.x, trans->pos.y);
+                std::cout<<trans->pos.x<<" "<<trans->pos.y<<"\n";
+                b2PolygonShape *dynamicBox = new b2PolygonShape();
+                dynamicBox->SetAsBox(sprite_width, sprite_height);
                 if (component["body"] == "dynamic")
                 {
-                    plat::Transform *trans = new_lvl.entities.back().getComponent<plat::Transform>();
-                    plat::Sprite *spr = new_lvl.entities.back().getComponent<plat::Sprite>();
-                    plat::Camera *cam = new_lvl.entities[new_lvl.cur_camera].getComponent<plat::Camera>();
-
-                    plat::Physics *cur_phys = new plat::Physics();
                     cur_phys->bodyDef.type = b2_dynamicBody;
-
-                    int sprite_width = spr->image.width * trans->scale.x * cam->scale.x;
-                    int sprite_height = spr->image.height * trans->scale.y * cam->scale.y;
-                    cur_phys->bodyDef.position.Set(trans->pos.x, trans->pos.y);
                     cur_phys->body = new_lvl.entities[new_lvl.cur_world].getComponent<plat::World>()->cur_world->CreateBody(&cur_phys->bodyDef);
 
-                    b2PolygonShape *dynamicBox = new b2PolygonShape();
-                    dynamicBox->SetAsBox(sprite_width, sprite_height);
+                    
 
                     b2FixtureDef *fixtureDef = new b2FixtureDef();
                     fixtureDef->shape = dynamicBox;
                     fixtureDef->density = 1.0f;
                     fixtureDef->friction = 0.3f;
                     cur_phys->body->CreateFixture(fixtureDef);
-                    new_lvl.entities.back().components.push_back(cur_phys);
                 }
+                else
+                {
+                    cur_phys->body = new_lvl.entities[new_lvl.cur_world].getComponent<plat::World>()->cur_world->CreateBody(&cur_phys->bodyDef);   
+                    cur_phys->body->CreateFixture(dynamicBox,0.0f);
+                }
+                    new_lvl.entities.back().components.push_back(cur_phys);
+                
             }
             else if (component["type"] == "Camera")
             {
